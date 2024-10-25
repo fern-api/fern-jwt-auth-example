fac5134ab73698e922da6f1f5b7ee107c17e4073
Andrew Jiang <andrew@buildwithfern.com>
2024-10-24 20:46:30 -0400

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,12 @@ export default function Home(props: HomeProps) {
 
           <div className="flex flex-col gap-4">
             <div className="flex items-center justify-center">
-              <input id="everyone" checked disabled />
+              <input
+                id="everyone"
+                checked
+                disabled
+                className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+              />
               <label htmlFor="orphaned">Orphaned</label>
             </div>
 
